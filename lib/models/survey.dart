e303d0b5bc8780b5b5f8a174f59bfb25675a8fee
PJ <peter.wdj@gmail.com>
2023-04-25 10:40:47 -0600

    Updates some survey attributes to better reflect survey states

--- lib/models/survey.dart
+++ lib/models/survey.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/foundation.dart';
 
+enum SurveyState { unstarted, inProgress, completed }
+
 class Survey with DiagnosticableTreeMixin {
-  final DateTime startAt;
-  final DateTime? endAt;
+  DateTime? startAt;
+  DateTime? endAt;
   final String? weather;
   final List<String> leaders;
   final String scribe;
   final List<String> participants;
   final String trail;
+  SurveyState state;
 
   Survey(
-      {required this.startAt,
-      required this.trail,
+      {required this.trail,
       required this.leaders,
       required this.scribe,
+      required this.participants,
+      this.startAt,
       this.endAt,
       this.weather,
-      this.participants = const []});
+      this.state = SurveyState.unstarted});
+
+  void start() {
+    startAt = DateTime.now();
+    state = SurveyState.inProgress;
+  }
 
   @override
   void debugFillProperties(DiagnosticPropertiesBuilder properties) {
@@ -25,10 +34,10 @@ class Survey with DiagnosticableTreeMixin {
     // See the documentation of debugFillProperties for more information.
     properties.add(StringProperty('startAt', startAt.toString()));
     properties.add(StringProperty('endAt', endAt?.toString()));
-    properties.add(StringProperty('trail', trail));
     properties.add(IterableProperty('leaders', leaders));
     properties.add(StringProperty('scribe', scribe));
     properties.add(IterableProperty('participants', participants));
     properties.add(StringProperty('weather', weather));
+    properties.add(StringProperty('trail', trail));
   }
 }
