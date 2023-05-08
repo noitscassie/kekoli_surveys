7834df056ca3fdbddb4c612f4ab977460986a118
PJ <peter.wdj@gmail.com>
2023-05-08 07:50:43 -0600

    Tweak time formatting a little

--- lib/utils/time_utils.dart
+++ lib/utils/time_utils.dart
@@ -2,10 +2,10 @@ class DateFormats {
   static ddmmyyyy(DateTime date) => '${date.day}/${date.month}/${date.year}';
 }
 
-class DurationFormats {
-  static hoursAndMinutes(Duration duration) {
-    final hours = duration.inHours;
-    final minutes = duration.inMinutes - (hours * Duration.minutesPerHour);
+class TimeFormats {
+  static hoursAndMinutesFromMinutes(int totalMinutes) {
+    final hours = (totalMinutes ~/ Duration.minutesPerHour);
+    final minutes = totalMinutes % Duration.minutesPerHour;
 
     return '${hours > 0 ? '$hours ${hours == 1 ? 'hour' : 'hours'} and ' : ''}$minutes ${minutes == 1 ? 'minute' : 'minutes'}';
   }
