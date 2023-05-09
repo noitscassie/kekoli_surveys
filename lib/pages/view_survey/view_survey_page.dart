ce1c2db125accc162ab1fa4c8ff0e52febf09492
PJ <peter.wdj@gmail.com>
2023-05-08 19:41:53 -0600

    Tidy up the view survey page a little

--- lib/pages/view_survey/view_survey_page.dart
+++ lib/pages/view_survey/view_survey_page.dart
@@ -2,14 +2,22 @@ import 'package:dartx/dartx.dart';
 import 'package:flutter/material.dart';
 import 'package:kekoldi_surveys/models/sighting.dart';
 import 'package:kekoldi_surveys/models/survey.dart';
+import 'package:kekoldi_surveys/pages/export_survey/export_survey_page.dart';
 import 'package:kekoldi_surveys/pages/view_survey/sighting_list_item.dart';
 import 'package:kekoldi_surveys/utils/time_utils.dart';
+import 'package:kekoldi_surveys/widgets/data_tile.dart';
 import 'package:kekoldi_surveys/widgets/page_scaffold.dart';
 import 'package:kekoldi_surveys/widgets/partly_bolded_text.dart';
 
 class ViewSurveyPage extends StatelessWidget {
   final Survey survey;
   const ViewSurveyPage({super.key, required this.survey});
+
+  String get participantsString => [
+        ...survey.leaders.map((String leader) => '$leader (leader)'),
+        '${survey.scribe} (scribe)',
+        ...survey.participants
+      ].join(', ');
 
   @override
   Widget build(BuildContext context) {
@@ -18,47 +26,52 @@ class ViewSurveyPage extends StatelessWidget {
       fabLabel: Row(
         children: const [Text('Export Data'), Icon(Icons.download)],
       ),
-      onFabPress: () {},
-      isFabValid: false,
+      onFabPress: () => Navigator.of(context).push(MaterialPageRoute(
+          builder: (BuildContext context) => ExportSurveyPage(survey: survey))),
       child: Padding(
         padding: const EdgeInsets.only(top: 16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            PartlyBoldedText(
-                style: Theme.of(context).textTheme.bodyMedium,
-                textParts: [
-                  RawText('Led by '),
-                  RawText(survey.leaders.join(' and '), bold: true)
-                ]),
-            PartlyBoldedText(
-              style: Theme.of(context).textTheme.bodyMedium,
-              textParts: [
-                RawText('Scribed by '),
-                RawText(survey.scribe, bold: true)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                DataTile(
+                    data: survey.sightings.length.toString(),
+                    label: 'Total Observations'),
+                DataTile(
+                    data: survey.sightings
+                        .map((Sighting sighting) => sighting.species)
+                        .distinct()
+                        .length
+                        .toString(),
+                    label: 'Unique Species'),
               ],
             ),
-            PartlyBoldedText(
-                style: Theme.of(context).textTheme.bodyMedium,
-                textParts: [
-                  RawText('Participated in by '),
-                  RawText(survey.participants.join(', '), bold: true),
-                ]),
-            PartlyBoldedText(
-                style: Theme.of(context).textTheme.bodyMedium,
-                textParts: [
-                  RawText('Survey began at '),
-                  RawText(TimeFormats.timeHoursAndMinutes(survey.startAt!),
-                      bold: true),
-                  RawText(' and ended at '),
-                  RawText(TimeFormats.timeHoursAndMinutes(survey.endAt!),
-                      bold: true),
-                  RawText(', lasting for '),
-                  RawText(
-                      TimeFormats.hoursAndMinutesFromMinutes(
-                          survey.lengthInMinutes()),
-                      bold: true),
-                ]),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  DataTile(
+                      data: TimeFormats.timeHoursAndMinutes(survey.startAt!),
+                      label: 'Start Time'),
+                  DataTile(
+                      data: TimeFormats.timeHoursAndMinutes(survey.endAt!),
+                      label: 'End Time'),
+                  DataTile(
+                      data: TimeFormats.hmFromMinutes(survey.lengthInMinutes()),
+                      label: 'Duration'),
+                ],
+              ),
+            ),
+            Text(
+              participantsString,
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyMedium
+                  ?.copyWith(fontStyle: FontStyle.italic),
+            ),
             PartlyBoldedText(
                 style: Theme.of(context).textTheme.bodyMedium,
                 textParts: [
@@ -66,15 +79,11 @@ class ViewSurveyPage extends StatelessWidget {
                   RawText(survey.weather!.toLowerCase(), bold: true),
                 ]),
             Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 8),
-                child: ListView(
-                  // shrinkWrap: true,
-                  children: List.from(survey.sightings
-                      .sortedBy((Sighting sighting) => sighting.species)
-                      .map((Sighting sighting) =>
-                          SightingListItem(sighting: sighting))),
-                ),
+              child: ListView(
+                children: List.from(survey.sightings
+                    .sortedBy((Sighting sighting) => sighting.species)
+                    .map((Sighting sighting) =>
+                        SightingListItem(sighting: sighting))),
               ),
             )
           ],
