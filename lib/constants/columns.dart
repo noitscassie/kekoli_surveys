e264cc48114c5099dd73c2a7d97e626a2f253d0c
PJ <peter.wdj@gmail.com>
2023-05-16 14:04:09 -0600

    Define the headers as a constant in preparation for making them configurable

---
+++ lib/constants/columns.dart
@@ -0,0 +1,16 @@
+final List<String> columns = [
+  'Species',
+  'scientific name',
+  'family',
+  'order',
+  'class',
+  'IUCN status',
+  'Migratory',
+  'Habitat Preferred',
+  '#',
+  'Height',
+  'Substrate',
+  'Sex',
+  'Adult/Juvenile',
+  'Type of Obs'
+];
