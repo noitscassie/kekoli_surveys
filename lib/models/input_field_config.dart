ef8593e72936ae31c90393724fc862601c3b995b
PJ <peter.wdj@gmail.com>
2023-05-17 20:15:34 -0600

    Extends input field config with an identifier and an option to sort the list of options or not

--- lib/models/input_field_config.dart
+++ lib/models/input_field_config.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:dartx/dartx.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:kekoldi_surveys/widgets/shared/multiline_text_input_field.dart';
@@ -7,6 +8,7 @@ import 'package:kekoldi_surveys/widgets/shared/number_input_field.dart';
 import 'package:kekoldi_surveys/widgets/shared/radio_buttons_input_field.dart';
 import 'package:kekoldi_surveys/widgets/shared/select_dropdown_input_field.dart';
 import 'package:kekoldi_surveys/widgets/shared/text_input_field.dart';
+import 'package:uuid/uuid.dart';
 
 enum FieldType {
   text,
@@ -17,56 +19,79 @@ enum FieldType {
 }
 
 class InputFieldConfig with DiagnosticableTreeMixin {
-  final String label;
-  final FieldType type;
-  final String defaultValue;
-  final List<String> options;
-  final bool required;
+  final String id;
+  String label;
+  FieldType type;
+  String defaultValue;
+  List<String?> options;
+  bool required;
+  bool sortOptions;
+
+  static const typesRequiringOptions = [
+    FieldType.radioButtons,
+    FieldType.select
+  ];
 
   InputFieldConfig.text(
       {required this.label, this.defaultValue = '', this.required = false})
       : type = FieldType.text,
-        options = [];
+        options = [],
+        sortOptions = true,
+        id = const Uuid().v4();
 
   InputFieldConfig.multilineText(
       {required this.label, this.defaultValue = '', this.required = false})
       : type = FieldType.multilineText,
-        options = [];
+        options = [],
+        sortOptions = true,
+        id = const Uuid().v4();
 
   InputFieldConfig.number(
       {required this.label, this.defaultValue = '1', this.required = false})
       : type = FieldType.number,
-        options = [];
+        options = [],
+        sortOptions = true,
+        id = const Uuid().v4();
 
   InputFieldConfig.radioButtons(
       {required this.label,
       required this.options,
       this.defaultValue = '',
-      this.required = false})
-      : type = FieldType.radioButtons;
+      this.required = false,
+      this.sortOptions = true})
+      : type = FieldType.radioButtons,
+        id = const Uuid().v4();
 
   InputFieldConfig.select(
       {required this.label,
       required this.options,
       this.defaultValue = '',
-      this.required = false})
-      : type = FieldType.select;
+      this.required = false,
+      this.sortOptions = true})
+      : type = FieldType.select,
+        id = const Uuid().v4();
 
   InputFieldConfig.fromJson(Map<String, dynamic> json)
-      : label = json['label'],
+      : id = json['id'],
+        label = json['label'],
         type = FieldType.values.byName(json['type']),
         defaultValue = json['defaultValue'],
         options = List<String>.from(json['options']),
-        required = json['required'];
+        required = json['required'],
+        sortOptions = json['sortOptions'] ?? true;
+
+  bool get requiresOptions => typesRequiringOptions.contains(type);
 
   String toJson() => jsonEncode(attributes);
 
   Map<String, dynamic> get attributes => {
+        'id': id,
         'label': label,
         'type': type.name,
         'defaultValue': defaultValue,
         'options': options,
         'required': required,
+        'sortOptions': sortOptions,
       };
 
   Widget inputField({required Function(String value) onChange, String? value}) {
@@ -95,13 +120,17 @@ class InputFieldConfig with DiagnosticableTreeMixin {
         return RadioButtonsInputField(
             label: label,
             value: fieldValue,
-            options: options,
+            options: (sortOptions ? options.sorted() : options)
+                .whereNotNull()
+                .toList(),
             onChange: onChange);
       case FieldType.select:
         return SelectDropdownInputField(
             label: label,
             value: fieldValue,
-            options: options,
+            options: (sortOptions ? options.sorted() : options)
+                .whereNotNull()
+                .toList(),
             onChange: onChange);
     }
   }
@@ -112,7 +141,8 @@ class InputFieldConfig with DiagnosticableTreeMixin {
     properties.add(StringProperty('label', label));
     properties.add(EnumProperty<FieldType>('type', type));
     properties.add(StringProperty('defaultValue', defaultValue));
-    properties.add(IterableProperty<String>('options', options));
+    properties.add(IterableProperty<String>('options', options.whereNotNull()));
     properties.add(DiagnosticsProperty<bool>('required', required));
+    properties.add(StringProperty('id', id));
   }
 }
